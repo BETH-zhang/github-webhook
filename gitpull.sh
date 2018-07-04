bf3b0b33c7c3426d7a2ca0869ab293374ddb02f1
BETH-zhang <522718277@qq.com>
2018-07-04 22:18:32 +0800

    add github-webhook

---
+++ gitpull.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# cd /var/www/project/jijiangshe
+# git checkout master
+# git pull origin master
+
+# 停止对应docker
+# docker stop 87d5eafe40a4
+
+# docker run -d -p 80:8888 7e749fbde3ed 
+
+WEB_PATH='/var/www/project/'$1
+WEB_USER='root'
+WEB_USERGROUP='root'
+
+echo "Start deployment"
+cd $WEB_PATH
+echo "pulling source code..."
+git reset --hard origin/master
+git clean -f
+git pull
+git checkout master
+echo "changing permissions..."
+chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
+echo "Finished."
